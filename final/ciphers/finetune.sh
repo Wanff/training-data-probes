f956903812f63083b3440bd172e0e4e2543546e2
Oam Patel <oampatel114@gmail.com>
2024-04-02 22:45:43 +0000

    back to oam

--- final/ciphers/finetune.sh
+++ final/ciphers/finetune.sh
@@ -1,6 +1,14 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=1
 
-python finetune_manual.py
+python finetune_manual.py \
+    --path "/home/ubuntu/gld/train-data-probes/data/70m/ciphers" \
+    --model_name "EleutherAI/pythia-70m" \
+    --dataset_name "rotated_7" \
+    --output_name "rotated_7_model" \
+    --seed 0 \
+    --lr 5e-5 \
+    --batch_size 16 \
+    --num_epochs 2 \
 
 # python finetune.py \
 #     --path "/home/ubuntu/gld/train-data-probes/data/70m/ciphers" \
